b92e634ff4f6fe1717b457874dd0aadf90b45f6b
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-05-14 17:40:15 -0500

    se agrego una prueba por modulo y se agrego el código para pasarlas, ya que en conjunto se agura aún más su funcionamiento

--- test/lib/controllers/InfoAnimalController.test.js
+++ test/lib/controllers/InfoAnimalController.test.js
@@ -2,7 +2,7 @@ const InfoAnimalController = require("../../../lib/Controllers/InfoAnimalControl
 
 describe("test para controller",() => {
     test("1) mostrar ejemplares en la BD provicional", () => {
-        const animal1 = {
+        const animal1 = [{
             name: "Tortuga caguama",
             nickName: "tortuga boba",
             from: "México",
@@ -10,7 +10,7 @@ describe("test para controller",() => {
             location: "Baja California, y llega a los estados de Quintana Roo, Campeche, Yucatán, Veracruz, Tabasco y Tamaulipas",
             info: "la pesca inadecuada, el robo de sus huevos y la degradación de las playas, la ha puesto en peligro de extinción",
             type: "Cheloniidae"
-        }
+        }]
         const animalForName = InfoAnimalController.show("Tortuga caguama");
         expect(animalForName).toStrictEqual(animal1);
     });
