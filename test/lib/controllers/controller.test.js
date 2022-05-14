20ad4897f68299ae28bae975711c18d269cc0b56
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-05-14 16:22:22 -0500

    agrego la prueba vacia del controller

---
+++ test/lib/controllers/controller.test.js
@@ -0,0 +1,8 @@
+const controller = require("../../../lib/Controllers/controller");
+
+describe("test para controller",() => {
+    test("1) mostrar ejemplares en la BD provicional", () => {
+        
+        expect().toBe();
+    });
+});
