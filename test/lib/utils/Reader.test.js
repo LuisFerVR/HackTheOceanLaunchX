e0c35751382a07aecf8c2f5157a5bca28224edae
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-05-14 14:14:25 -0500

    agregar nuestra primer prueba vacia

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,7 @@
+const reader = require("../../../lib/utils/Reader");
+
+dsecribe("prueba para comprobar la lectura de nuestra BD provicional",() =>{
+    test("contar los ejemplares en ella", () => {
+
+    })
+})
