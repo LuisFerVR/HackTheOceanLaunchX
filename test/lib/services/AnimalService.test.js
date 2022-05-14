03b4b08aba06429a5d4f99396d37e1924da3e8ff
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-05-14 17:34:27 -0500

    se agrego el código necesario para Service y el filtrado del ejemplar buscado

---
+++ test/lib/services/AnimalService.test.js
@@ -0,0 +1,15 @@
+const AnimalService = require("../../../lib/services/AnimalService");
+ 
+describe("Tests para AnimalService", () => {
+    test("", () => {
+        const animal = [{name: "Tortuga caguama",
+        nickName: "tortuga boba",
+        from: "México",
+        peculiarity: "su cabeza mide aproximadamente 25cm",
+        location: "Baja California, y llega a los estados de Quintana Roo, Campeche, Yucatán, Veracruz, Tabasco y Tamaulipas",
+        info: "la pesca inadecuada, el robo de sus huevos y la degradación de las playas, la ha puesto en peligro de extinción",
+        type: "Cheloniidae"}];
+        var animalName = AnimalService.filterByName(animal, "Tortuga caguama");
+        expect(animalName).toStrictEqual(animal);
+    });
+});
