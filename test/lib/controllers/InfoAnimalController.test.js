475fae69d9297bb0bc7d757888a1d1729458aca1
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-05-14 16:38:47 -0500

    agregando codigo necesario para pasar la prueba

---
+++ test/lib/controllers/InfoAnimalController.test.js
@@ -0,0 +1,17 @@
+const InfoAnimalController = require("../../../lib/Controllers/InfoAnimalController");
+
+describe("test para controller",() => {
+    test("1) mostrar ejemplares en la BD provicional", () => {
+        const animal1 = {
+            name: "Tortuga caguama",
+            nickName: "tortuga boba",
+            from: "México",
+            peculiarity: "su cabeza mide aproximadamente 25cm",
+            location: "Baja California, y llega a los estados de Quintana Roo, Campeche, Yucatán, Veracruz, Tabasco y Tamaulipas",
+            info: "la pesca inadecuada, el robo de sus huevos y la degradación de las playas, la ha puesto en peligro de extinción",
+            type: "Cheloniidae"
+        }
+        const animalForName = InfoAnimalController.show("Tortuga caguama");
+        expect(animalForName).toStrictEqual(animal1);
+    });
+});
--- test/lib/controllers/controller.test.js
+++
@@ -1,8 +0,0 @@
-const controller = require("../../../lib/Controllers/controller");
-
-describe("test para controller",() => {
-    test("1) mostrar ejemplares en la BD provicional", () => {
-        
-        expect().toBe();
-    });
-});
